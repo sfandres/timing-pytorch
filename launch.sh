1b9fa4345c73672ab93fc7da3eeffbb6d66892da
sfandres94 <andres.sanchez1994@gmail.com>
2023-05-17 15:09:15 +0200

    Add mem=0 and --exclusive for timing

--- launch.sh
+++ launch.sh
@@ -6,10 +6,13 @@
 #SBATCH --nodes=1                                   ## Number of nodes.
 #SBATCH --ntasks=1                                  ## Number of tasks.
 #SBATCH --ntasks-per-node=1                         ## Number of tasks to be invoked on each node.
+#SBATCH --threads-per-core=1                        ## Restrict node selection to nodes with at least the specified number of threads per core.
 #SBATCH --cpus-per-task=8                           ## Number of cpu-cores per task (>1 if multi-threaded tasks).
-#SBATCH --gpus-per-node=1                           ## Number of GPUs on each node (Sergio: 4).
-#SBATCH --mail-type=ALL                             ## (not working) Type of notification via email.
-#SBATCH --mail-user=sfandres@unex.es                ## (not working) User to receive the email notification.
+#SBATCH --gpus-per-node=1                           ## Number of GPUs on each node.
+#SBATCH --mem=0                                     ## Real memory required per node (0: request all the memory on a node).
+#SBATCH --exclusive                                 ## Job allocation can not share nodes with other running jobs.
+#SBATCH --mail-type=ALL                             ## Type of notification via email.
+#SBATCH --mail-user=sfandres@unex.es                ## User to receive the email notification.
 
 ## Catch Slurm environment variables.
 job_id=${SLURM_JOB_ID}
@@ -29,4 +32,4 @@ echo "srun python3 timing_training_loop.py "$@""
 srun python3 timing_training_loop.py "$@"
 
 ## Send email when job ends.
-/usr/bin/mail -a timing_${job_id}.out -s "Sbatch ${email_info} ended" sfandres@unex.es
+echo " " | /usr/bin/mail -s "Sbatch ${email_info} ended" sfandres@unex.es
