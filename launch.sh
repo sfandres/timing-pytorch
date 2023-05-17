080cf13d0847ce1adf4e7d508bd6f2dc8b08916a
sfandres94 <andres.sanchez1994@gmail.com>
2023-05-17 23:03:51 +0200

    Fix bug

--- launch.sh
+++ launch.sh
@@ -6,8 +6,8 @@
 #SBATCH --nodes=1                                   ## Number of nodes.
 #SBATCH --ntasks=1                                  ## Number of tasks.
 #SBATCH --ntasks-per-node=1                         ## Number of tasks to be invoked on each node.
-##SBATCH --cpus-per-task=6                          ## Number of cpu-cores per task (>1 if multi-threaded tasks).
-#SBATCH --cpus-per-gpu=6                            ## Number of cpu-cores per task (>1 if multi-threaded tasks).
+#SBATCH --cpus-per-task=4                           ## Number of cpu-cores per task (>1 if multi-threaded tasks).
+##SBATCH --cpus-per-gpu=6                           ## Number of cpu-cores per task (>1 if multi-threaded tasks).
 #SBATCH --threads-per-core=1                        ## Restrict node selection to nodes with at least the specified number of threads per core.
 #SBATCH --gpus-per-node=1                           ## Min. number of GPUs on each node.
 #SBATCH --mem=0                                     ## Real memory required per node (0: request all the memory on a node).
